0fbccd85c9bc0a2939ff5efd197107a93137d133
Afonso Henrique <hafonso021@gmail.com>
2022-07-13 08:14:33 -0300

    feat(Cep): Update

--- src/pages/Cep.js
+++ src/pages/Cep.js
@@ -1,9 +1,13 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
+
 
 export default props => {
     return(
         <View>
             <Text>Tela Cep</Text>
+            <TextInput placeholder="CEP somente números" />
+            <Button title="Pesquisar" />
+            <Text>Logradouro: </Text>
         </View>
     )
 }
