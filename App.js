6de860a7e67bd2904af525fd5e87a955493eba8c
Afonso Henrique <hafonso021@gmail.com>
2022-07-13 08:04:45 -0300

    feat(App): Update

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ const Tab = createBottomTabNavigator();
 export default () => {
   return (
     <NavigationContainer>
-      
+      <Tab.Navigator initialRouteName="Index">
+        <Tab.Screen name="Index" component={Index} />
+        <Tab.Screen name="Cep" component={Cep} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
